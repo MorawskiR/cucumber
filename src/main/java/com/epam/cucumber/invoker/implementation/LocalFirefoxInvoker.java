813b37ff7beeb78844cd4e241f18b57771aa7566
Kostiantyn Gavura <gavura@duck.com>
2022-03-27 16:52:28 +0200

    complete configurations

--- src/main/java/com/epam/cucumber/invoker/implementation/LocalFirefoxInvoker.java
+++ src/main/java/com/epam/cucumber/invoker/implementation/LocalFirefoxInvoker.java
@@ -1,16 +1,16 @@
 package com.epam.cucumber.invoker.implementation;
 
-//import io.github.bonigarcia.wdm.WebDriverManager;
 import com.epam.cucumber.invoker.WebDriverInvoker;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LocalFirefoxInvoker implements WebDriverInvoker {
     @Override
     public WebDriver invokeWebDriver() {
-//        WebDriverManager.firefoxdriver()
-//                .browserVersion("99")
-//                .setup();
+        WebDriverManager.firefoxdriver()
+                .browserVersion("latest")
+                .setup();
 
         return new FirefoxDriver();
     }
